e41880c9f87ff48bbe51b19dd967b0fdfa25a569
Ron V <ronneyvargas@gmail.com>
2019-11-03 14:01:02 -0600

    Updates

---
+++ precache-manifest.4525237bdddf904d06e9f2316db1b1b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57f01057482828687dc3fee6c147bd9",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "9a59c8fbe9a0c7d8bab6",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "694b47f258a79ee86926",
+    "url": "/robofriends/static/css/main.4a213b8a.chunk.css"
+  },
+  {
+    "revision": "9a59c8fbe9a0c7d8bab6",
+    "url": "/robofriends/static/js/2.4e5b532c.chunk.js"
+  },
+  {
+    "revision": "694b47f258a79ee86926",
+    "url": "/robofriends/static/js/main.2447f4f4.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
